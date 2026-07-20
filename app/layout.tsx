c3fbc4b7730bbdf0e3ad78f3e4e31b4a28221d2f
Ameen Ahmed <iameenahmed01@gmail.com>
2026-07-20 15:06:55 +0500

    fix(layout): add suppressHydrationWarning to RootLayout and handle resolvedTheme null case in ThemeToggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${jakartaSans.variable} font-bold antialiased`}>
         <ThemeProvider
           attribute="class"
--- components/layout/theme-toggle.tsx
+++ components/layout/theme-toggle.tsx
@@ -8,14 +8,16 @@ import { Switch } from "../ui/switch";
 export const ThemeToggle = () => {
   const { setTheme, resolvedTheme } = useTheme();
 
-  const isDark = resolvedTheme === "dark";
+  if (!resolvedTheme) {
+    return null;
+  }
 
   return (
     <div className="text-medium-grey bg-light-grey hover:bg-light-grey dark:bg-very-dark-grey mx-4 flex h-12 items-center justify-center gap-4 rounded-md">
       <Sun className="size-5 cursor-pointer" />
       <Switch
         id="toggle-theme"
-        checked={isDark}
+        checked={resolvedTheme === "dark"}
         onCheckedChange={(checked) => setTheme(checked ? "dark" : "light")}
         aria-label="Toggle dark mode"
         className="bg-purple cursor-pointer"
